08b74a63dc36f51dba4366e2a5b9a2faefab15f8
Matt Hollingsworth <matt@nontrivialexercises.com>
2011-11-15 12:11:40 -0600

    first cascading drop down working part 2

---
+++ Benchmark Instant Reports 2/CascadingDropDown1.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="CascadingDropDown1.asmx.cs" Class="Benchmark_Instant_Reports_2.CascadingDropDown1" %>
---
+++ Benchmark Instant Reports 2/CascadingDropDown1.asmx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using AjaxControlToolkit;
+using System.Data;
+
+namespace Benchmark_Instant_Reports_2
+{
+    /// <summary>
+    /// Summary description for CascadingDropDown1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class CascadingDropDown1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public CascadingDropDownNameValue[] GetCampusList(string knownCategoryValues, string category)
+        {
+            List<CascadingDropDownNameValue> returnList = new List<CascadingDropDownNameValue>();
+            DataSet ds = dbIFOracle.getDataRows(birIF.getCampusListQuery);
+
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                returnList.Add(new CascadingDropDownNameValue(row["SCHOOLNAME"].ToString(), row["SCHOOL_ABBR"].ToString()));
+            }
+
+            return returnList.ToArray();
+        }
+    }
+}
