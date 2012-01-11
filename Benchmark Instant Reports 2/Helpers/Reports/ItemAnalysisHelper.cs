3aea7405fd54763cb7c89790e1e96ac9ced4605c
Matt Hollingsworth <matt@nontrivialexercises.com>
2012-01-11 11:43:51 -0600

    concluded perf analysis - db read/writes are the bad

--- Benchmark Instant Reports 2/Helpers/Reports/ItemAnalysisHelper.cs
+++ Benchmark Instant Reports 2/Helpers/Reports/ItemAnalysisHelper.cs
@@ -9,6 +9,7 @@ using Benchmark_Instant_Reports_2.Grading;
 using Benchmark_Instant_Reports_2.References;
 using System.Configuration;
 using System.Data.OracleClient;
+using System.Collections;
 
 namespace Benchmark_Instant_Reports_2.Helpers.Reports
 {
@@ -16,86 +17,76 @@ namespace Benchmark_Instant_Reports_2.Helpers.Reports
     {
         public static List<ResultsTableItem> generateBenchmarkStatsRepTableQ(List<StudentScanDataItem> studentData, string curTest, string curCampus)
         {
-            //DataView dvStudentData = new DataView(studentData);
             string curId;
-            //DataSet dsFinal = new DataSet();
             List<ResultsTableItem> finalData = new List<ResultsTableItem>();
-            List<ResultsTableItem> resultsData = new List<ResultsTableItem>();
-            //DataTable resultsTable = new DataTable();
+            //List<ResultsTableItem> resultsData = new List<ResultsTableItem>();
+            Hashtable resultsDataH = new Hashtable();
             int curNumCorrect, curNumTotal = new int();
             AnswerCounter curAnsCount = new AnswerCounter();
 
-            //setupResultsDataTable(resultsTable);
-
             // grade each student's test and add it to the DataSet
             int numNull = 0;
-            //for (int jj = 0; jj < dvStudentData.Table.Rows.Count; jj++)
+            //// try using foreach (StudentSCanDataItem curStudentScan in studentData)
             for (int jj = 0; jj < studentData.Count; jj++)
             {
-                //curId = dvStudentData.Table.Rows[jj]["local_student_id"].ToString();
+                //// just use curStudentScan.StudentID
                 curId = studentData[jj].StudentID;
 
-                //DataRow curScanDataRow = birIF.getLatestScanDataRow(curId, curTest);
+                //// this is a db hit for every student; let's just do 1 query to get current scan data rows for each of the students in the list?
                 ScanItem curScanDataItem = birIF.getLatestScanDataRowq(curId, curTest);
-                //if (curScanDataRow != null)
                 if (curScanDataItem != null)
                 {
-                    //DataTable gradedTable = birIF.gradeScannedTestDetail(curTest, curScanDataRow["ANSWERS"].ToString(), curCampus, 0,
-                    //    dvStudentData.Table.Rows[jj][Constants.TeacherNameFieldName].ToString(),
-                    //    dvStudentData.Table.Rows[jj]["PERIOD"].ToString());
                     List<GradedItemDetail> gradedData = GradeTest.gradeScannedTestDetailQ(curTest, curScanDataItem.Answers, curCampus, 0,
                         studentData[jj].TeacherName, studentData[jj].Period);
 
                     
                     // add data for each test item to the resultsTable
-                    //for (int k = 0; k < gradedTable.Rows.Count; k++)
                     for (int k = 0; k < gradedData.Count; k++)
                     {
                         // see if this item is already in the dataset
-                        //string selectString =
-                        //    //"CAMPUS = \'" + dvStudentData.Table.Rows[j]["SCHOOL_ABBR"].ToString() + "\' and " +
-                        //    "CAMPUS = \'" + dvStudentData.Table.Rows[jj]["SCHOOL2"].ToString() + "\' and " +
-                        //    "TEST_ID = \'" + curTest + "\' and " +
-                        //    "TEACHER = \'" + dvStudentData.Table.Rows[jj][Constants.TeacherNameFieldName].ToString().Replace("'", "''") + "\' and " +
-                        //    "PERIOD = \'" + dvStudentData.Table.Rows[jj]["PERIOD"].ToString() + "\' and " +
-                        //    "ITEM_NUM = " + gradedTable.Rows[k]["ITEM_NUM"];
-                        //DataRow[] selectedRows = resultsTable.Select(selectString);
+                        ResultsTableItem findthis = new ResultsTableItem(studentData[jj].Campus, curTest, studentData[jj].TeacherName,
+                                                                         studentData[jj].Period, gradedData[k].ItemNum);
+                        ResultsTableItemKey findthiskey = new ResultsTableItemKey(findthis);
+                        if (resultsDataH.ContainsKey(findthiskey))
+                        
 
-                        List<ResultsTableItem> foundItems = finalData.FindAll(delegate(ResultsTableItem rti)
-                        {
-                            if (rti.Campus == studentData[jj].Campus &&
-                                rti.TestID == curTest &&
-                                rti.Teacher == studentData[jj].TeacherName &&
-                                rti.Period == studentData[jj].Period &&
-                                rti.ItemNum == gradedData[k].ItemNum)
-                                return true;
+                        //List<ResultsTableItem> foundItems = finalData.FindAll(delegate(ResultsTableItem rti)
+                        //{
+                        //    if (rti.Campus == studentData[jj].Campus &&
+                        //        rti.TestID == curTest &&
+                        //        rti.Teacher == studentData[jj].TeacherName &&
+                        //        rti.Period == studentData[jj].Period &&
+                        //        rti.ItemNum == gradedData[k].ItemNum)
+                        //        return true;
 
-                            return false;
-                        });
+                        //    return false;
+                        //});
 
-                        //if (selectedRows.Length > 0)
-                        if (foundItems.Count > 0)
+                        //if (foundItems.Count > 0)
                         {
                             // this item is here - get the values and then delete it
-                            //curNumCorrect = (int)selectedRows[0][lblNumCorrect];
-                            curNumCorrect = foundItems[0].NumCorrect;
-                            //curNumTotal = (int)selectedRows[0][lblNumTotal];
-                            curNumTotal = foundItems[0].NumTotal;
-                            //curAnsCount = getAnswerCounts(selectedRows[0]);
-                            curAnsCount.UpdateFromResultsTableItem(foundItems[0]);
+                            //curNumCorrect = foundItems[0].NumCorrect;
+                            //curNumTotal = foundItems[0].NumTotal;
+                            //curAnsCount.UpdateFromResultsTableItem(foundItems[0]);
 
-                            //resultsTable.Rows.Remove(selectedRows[0]);
-                            finalData.Remove(finalData.Find(delegate(ResultsTableItem rti)
-                            { 
-                                if (rti.TestID == foundItems[0].TestID &&
-                                    rti.Campus == foundItems[0].Campus &&
-                                    rti.Teacher == foundItems[0].Teacher &&
-                                    rti.Period == foundItems[0].Period &&
-                                    rti.ItemNum == foundItems[0].ItemNum)
-                                    return true;
+                            ResultsTableItem foundItem = resultsDataH[findthiskey] as ResultsTableItem;
+                            curNumCorrect = foundItem.NumCorrect;
+                            curNumTotal = foundItem.NumTotal;
+                            curAnsCount.UpdateFromResultsTableItem(foundItem);
 
-                                return false;
-                            }));
+                            //finalData.Remove(finalData.Find(delegate(ResultsTableItem rti)
+                            //{ 
+                            //    if (rti.TestID == foundItems[0].TestID &&
+                            //        rti.Campus == foundItems[0].Campus &&
+                            //        rti.Teacher == foundItems[0].Teacher &&
+                            //        rti.Period == foundItems[0].Period &&
+                            //        rti.ItemNum == foundItems[0].ItemNum)
+                            //        return true;
+
+                            //    return false;
+                            //}));
+
+                            resultsDataH.Remove(findthiskey);
                             
                         }
                         else
@@ -106,42 +97,27 @@ namespace Benchmark_Instant_Reports_2.Helpers.Reports
                         }
 
                         // add the row to the results table
-                        //DataRow thisrow = resultsTable.NewRow();
                         ResultsTableItem newItem = new ResultsTableItem();
-                        //thisrow[lblCampus] = dvStudentData.Table.Rows[j]["SCHOOL_ABBR"].ToString();
-                        //thisrow[lblCampus] = dvStudentData.Table.Rows[jj]["SCHOOL2"].ToString();
                         newItem.Campus = studentData[jj].Campus;
-                        //thisrow[lblTestId] = curTest;
                         newItem.TestID = curTest;
-                        //thisrow[lblTeacher] = dvStudentData.Table.Rows[jj][Constants.TeacherNameFieldName].ToString();
                         newItem.Teacher = studentData[jj].TeacherName;
-                        //thisrow[lblPeriod] = dvStudentData.Table.Rows[jj]["PERIOD"].ToString();
                         newItem.Period = studentData[jj].Period;
-                        //thisrow[lblItemNum] = gradedTable.Rows[k]["ITEM_NUM"];
                         newItem.ItemNum = gradedData[k].ItemNum;
-                        //if ((bool)gradedTable.Rows[k]["CORRECT"])
                         if (gradedData[k].Correct)
                             curNumCorrect++;
-                        //curAnsCount.Increment(gradedTable.Rows[k]["STUDENT_ANS"].ToString());
                         curAnsCount.Increment(gradedData[k].StudentAnswer);
                         curNumTotal++;
-                        //thisrow[lblNumCorrect] = curNumCorrect;
                         newItem.NumCorrect = curNumCorrect;
-                        //thisrow[lblNumTotal] = curNumTotal;
                         newItem.NumTotal = curNumTotal;
-                        //thisrow[lblPctCorrect] = (decimal)curNumCorrect / (decimal)curNumTotal;
                         newItem.PctCorrect = (decimal)curNumCorrect / (decimal)curNumTotal;
-                        //putAnswerCounts(curAnsCount, thisrow);
                         curAnsCount.UpdateToResultsTableItem(newItem);
-                        //thisrow[lblAnswer] = gradedTable.Rows[k]["CORRECT_ANS"].ToString();
                         newItem.Answer = gradedData[k].CorrectAnswer;
-                        //thisrow[lblObjective] = gradedTable.Rows[k]["OBJECTIVE"];
                         newItem.Objective = gradedData[k].Category;
-                        //thisrow[lblTEKS] = gradedTable.Rows[k]["TEKS"];
                         newItem.TEKS = gradedData[k].TEKS;
 
-                        //resultsTable.Rows.Add(thisrow);
-                        finalData.Add(newItem);
+                        //finalData.Add(newItem);
+                        ResultsTableItemKey newItemKey = new ResultsTableItemKey(newItem);
+                        resultsDataH.Add(newItemKey, newItem);
                     }
 
                 }
@@ -151,8 +127,8 @@ namespace Benchmark_Instant_Reports_2.Helpers.Reports
                 }
             }
 
-            //return resultsTable;
-            return finalData;
+            //return finalData;
+            return resultsDataH.Values.Cast<ResultsTableItem>().ToList();
         }
 
 
